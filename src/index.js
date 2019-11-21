f251b821c6ce265156725d49e5ddb58330068c92
clarkj99 <clark@stontech.net>
2019-11-21 15:51:05 -0500

    cards are litttt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let addToy = false
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
   addBtn.addEventListener('click', () => {
@@ -12,5 +12,31 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
+
+  const response = fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(data => {
+      let div = document.querySelector('#toy-collection');
+      data.forEach(toy => {
+        let card = document.createElement('div');
+        card.className = 'card';
+        let h2 = document.createElement('h2');
+        h2.innerText = toy.name;
+        let img = document.createElement('img');
+        img.src = toy.image
+        img.className = 'toy-avatar'
+        let p = document.createElement('p');
+        p.innerText = toy.likes + ' Likes'
+        let button = document.createElement('button');
+        button.className = 'like-btn'
+        button.innerText = 'Like <3'
+
+        card.appendChild(h2)
+        card.appendChild(img)
+        card.appendChild(p)
+        card.appendChild(button)
+        div.appendChild(card)
+      });
+    })
 
 })
